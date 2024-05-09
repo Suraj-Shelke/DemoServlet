8ae8f6cb85b9f468adc4cebb69b8f60233c8b342
Suraj-Shelke <shelkesuraj205@gmail.com>
2024-05-09 22:06:58 +0530

    first commit

---
+++ DemoServelet/src/main/java/com/sp/AddServlet.java
@@ -0,0 +1,28 @@
+package com.sp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int n1=Integer.parseInt(req.getParameter("num1"));
+		int n2=Integer.parseInt(req.getParameter("num2"));
+		int result=n1+n2;
+		//PrintWriter out=res.getWriter();
+		//out.print("Result is: "+result);
+		req.setAttribute("n1", n1);
+		req.setAttribute("n2",n2);
+		req.setAttribute("result", result);
+		RequestDispatcher rd=req.getRequestDispatcher("square");
+		rd.forward(req,res);
+	}
+
+}
---
+++ DemoServelet/src/main/java/com/sp/SquareServlet.java
@@ -0,0 +1,24 @@
+package com.sp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int n1=(int) req.getAttribute("n1");
+		int n2=(int) req.getAttribute("n2");
+		int result=(int) req.getAttribute("result");
+		result=result*result;
+		PrintWriter out=res.getWriter();
+		out.print("square of addition of the number "+n1+" and "+n2+" is : " +result);
+		
+	}
+
+}
+ 
