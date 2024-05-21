dcf9e4024a96c6b3d0a5c94de7bf7f425c038305
Suraj-Shelke <shelkesuraj205@gmail.com>
2024-05-21 21:14:07 +0530

    Servlet Annotation configuration

--- DemoServelet/src/main/java/com/sp/AddServlet.java
+++ DemoServelet/src/main/java/com/sp/AddServlet.java
@@ -4,29 +4,33 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
 	{
 		int n1=Integer.parseInt(req.getParameter("num1"));
 		int n2=Integer.parseInt(req.getParameter("num2"));
 		int result=n1+n2;
-		//PrintWriter out=res.getWriter();
-		//out.print("Result is: "+result);
-//		req.setAttribute("n1", n1);
-//		req.setAttribute("n2",n2);
-//		req.setAttribute("result", result);
+//		PrintWriter out=res.getWriter();
+//		out.print("Result is: "+result);
+		req.setAttribute("n1", n1);
+		req.setAttribute("n2",n2);
+		req.setAttribute("result", result);
 
 //      calling another servlet by using request dispatcher	
-//		RequestDispatcher rd=req.getRequestDispatcher("square");
-//		rd.forward(req,res);
+		RequestDispatcher rd=req.getRequestDispatcher("square");
+		rd.forward(req,res);
 		
 //		send redirect method
 //		res.sendRedirect("square?result="+result);
@@ -37,10 +41,25 @@ public class AddServlet extends HttpServlet {
 //		res.sendRedirect("square");
 	
 //		Cookies
-		Cookie cookie=new Cookie("result",result+"");
-		res.addCookie(cookie);
-		res.sendRedirect("square");
+//		Cookie cookie=new Cookie("result",result+"");
+//		res.addCookie(cookie);
+//		res.sendRedirect("square");
+//		
 		
-	}
+//		ServletConfig & ServletContext
+		
+//		PrintWriter out=res.getWriter();
+//		out.print("hi ");
+		
+//		ServletContext ctx=getServletContext();
+//		String name=ctx.getInitParameter("name");
+//		out.println(name);
+//	
+		
+//		ServletConfig cfg=getServletConfig();
+//		String name=cfg.getInitParameter("name");
+//		out.println(name);
+//		
+		}
 
 }
--- DemoServelet/src/main/java/com/sp/SquareServlet.java
+++ DemoServelet/src/main/java/com/sp/SquareServlet.java
@@ -3,19 +3,21 @@ package com.sp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/square")
 public class SquareServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
 	{
-//		int n1=(int) req.getAttribute("n1");
-//		int n2=(int) req.getAttribute("n2");
-//		int result=(int) req.getAttribute("result");
+		int n1=(int) req.getAttribute("n1");
+		int n2=(int) req.getAttribute("n2");
+		int result=(int) req.getAttribute("result");
 //		int result=Integer.parseInt(req.getParameter("result"));
 		
 //      getting session value and printing it
@@ -24,16 +26,16 @@ public class SquareServlet extends HttpServlet {
 //		result=result*result; 
 		
 //	    getting cookie and printing 
-		int result=0;
-		Cookie cookies[]=req.getCookies();
-		for(Cookie c:cookies)
-		{
-			if(c.getName().equals("result"))
-			{
-				result=Integer.parseInt(c.getValue());
-			}
-			
-		}
+//		int result=0;
+//		Cookie cookies[]=req.getCookies();
+//		for(Cookie c:cookies)
+//		{
+//			if(c.getName().equals("result"))
+//			{
+//				result=Integer.parseInt(c.getValue());
+//			}
+//			
+//		}
 		result=result*result; 
 		PrintWriter out=res.getWriter();
 		out.print("square of addition of the number is : " +result);
